ba201ae1e491f2d18dc61e4b40dffecc8a36e54c
Ward Minnis <ward@minnis.net>
2015-03-02 01:03:44 -0500

    inline templates are working

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router'])
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+}]);
 
 app.factory('posts', [function(){
   var o = {
     posts: []
   };
   return o;
-}])
+}]);
 
 app.controller('MainCtrl', [
   '$scope',
