e98d54e50760276dc7f0c3dea52ce0fb8e45c54d
Ward Minnis <ward@minnis.net>
2015-03-02 00:06:51 -0500

    upvoting works

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.controller('MainCtrl', [
       { title: 'post 5', upvotes: 4 },
     ];
     $scope.addPost = function(){
-      $scope.posts.push({title: 'A new post', upvotes: 0 });
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({title: $scope.title, upvotes: 0 });
+      $scope.title = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
     };
 }]);
