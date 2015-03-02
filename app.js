93899cfcaeebb04649055eac4d98d0ff393a0a03
Ward Minnis <ward@minnis.net>
2015-03-01 23:54:51 -0500

    one small step. A button has been added

--- app.js
+++ app.js
@@ -11,4 +11,7 @@ app.controller('MainCtrl', [
       { title: 'post 4', upvotes: 9 },
       { title: 'post 5', upvotes: 4 },
     ];
+    $scope.addPost = function(){
+      $scope.posts.push({title: 'A new post', upvotes: 0 });
+    };
 }]);
